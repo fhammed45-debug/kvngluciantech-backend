5604c0010003bb6c83475606ebb6b572c01dec46
Fatai Hammed <fhammed45-debug>
2025-11-29 00:31:57 +0100

    Fix model imports to use lowercase filenames

--- models/index.js
+++ models/index.js
@@ -1,10 +1,11 @@
 const sequelize = require('../config/database');
-const User = require('./User');
-const Product = require('./Product');
-const Order = require('./Order');
-const Subscription = require('./Subscription');
-const EmailVerification = require('./EmailVerification');
-const PasswordReset = require('./PasswordReset');
+const User = require('./user');
+const Product = require('./product');
+const Order = require('./order');
+const Subscription = require('./subscription');
+const EmailVerification = require('./emailVerification');
+const PasswordReset = require('./passwordReset');
+
 
 // Define Relationships
 User.hasMany(Order, { foreignKey: 'user_id', as: 'orders' });
